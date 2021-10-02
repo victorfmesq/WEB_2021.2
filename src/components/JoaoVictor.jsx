391a2db3c3d5bdb30b766ab20539f39ec2e76241
victorfmesq <victorfmesq@gmail.com>
2021-10-02 18:31:37 -0300

    add react app

---
+++ src/components/JoaoVictor.jsx
@@ -0,0 +1,11 @@
+export default function JoaoVictor(){
+    return(
+        <div>
+            <h3>Nome: João Victor Freitas de Mesquita</h3>
+            <h3>Idade: 21</h3>
+            <h3>Curso: Sistemas de Informação</h3>
+            <h3>Cidade de Origem: Fortaleza</h3>
+            <h3>E-mail: victorfmesq@gmail.com</h3>
+        </div>
+    );
+}
---
+++ src/components/OlaMundo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OlaMundo(){
+    return(
+        <div>
+            <h1>Olá mundo React!</h1>
+            esta é a primeira aula de React!
+        </div>
+    )
+}
+
+export default OlaMundo;
